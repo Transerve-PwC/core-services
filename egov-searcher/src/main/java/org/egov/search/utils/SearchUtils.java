617652b98e729f51e6540277db8856e3f0ce4ef6
Chaitanya <chaitanya.d@transerve.com>
2021-06-21 16:13:20 +0530

    Revert "Handling the like operator replacing issue."
    This reverts commit 8a54bb634b80de0544c0857055660c1efa806b3f.

--- egov-searcher/src/main/java/org/egov/search/utils/SearchUtils.java
+++ egov-searcher/src/main/java/org/egov/search/utils/SearchUtils.java
@@ -168,8 +168,7 @@ public class SearchUtils {
 					whereClause.append(param.getName()).append(" " + operator + " ").append(":" + param.getName());
 				}
 
-				if(!preparedStatementValues.containsKey(param.getName()))
-					preparedStatementValues.put(param.getName(), paramValue);
+				preparedStatementValues.put(param.getName(), paramValue);
 			}
 		} catch (Exception e) {
 			log.error("Exception while bulding query: ", e);
